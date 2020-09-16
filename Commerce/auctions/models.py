6a96aa761fce0323808aa49ff6fe82dce329e019
Roger Castro <icarus.gk@gmail.com>
2020-09-15 22:07:21 -0600

    Add models

--- Commerce/auctions/models.py
+++ Commerce/auctions/models.py
@@ -6,9 +6,16 @@ class User(AbstractUser):
     pass
 
 # Auction Listings
-
+class Listing(models.Model):
+    pass
 # Bids
+class Bid(models.Model):
+    pass
 
 # Comments
+class Comment(models.Model):
+    pass
 
 # Auction Categories
+class Category(models.Model):
+    pass
