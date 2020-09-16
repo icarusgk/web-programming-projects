83efc43df420ab45b2a3eeb3c2037f5e2d859d35
Roger Castro <icarus.gk@gmail.com>
2020-09-15 18:43:12 -0600

    Initial commit

---
+++ Commerce/auctions/models.py
@@ -0,0 +1,14 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    pass
+
+# Auction Listings
+
+# Bids
+
+# Comments
+
+# Auction Categories
