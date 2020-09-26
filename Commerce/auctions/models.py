7c8358c347a78da19637b5afa3a7239eae187fe3
Roger Castro <icarus.gk@gmail.com>
2020-09-26 17:34:20 -0600

    Change Models

--- Commerce/auctions/models.py
+++ Commerce/auctions/models.py
@@ -1,33 +1,32 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-
+# User 
 class User(AbstractUser):
     pass
 
 # Auction Categories
-
 class Category(models.Model):
-    listing = models.ForeignKey(Listing, on_delete=models.CASCADE)
-    category = models.CharField(max_length=50)
-
-# Bids
-class Bid(models.Model):
-    listing = models.ForeignKey(Listing, on_delete=models.CASCADE)
-    start_bid = models.FloatField()
-    user = models.ForeignKey(User, on_delete=models.CASCADE)    
-
-# Comments
-class Comment(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    comment = models.CharField(max_length=256)
-
-
+    name = models.CharField(max_length=50)
 
 # Listings
 class Listing(models.Model):
     product_name = models.CharField(max_length=256)
     description = models.CharField(max_length=256)
-    start_bid = models.FloatField(max_length=16)
-    image_url = models.URLField()
+    image_url = models.URLField()  
+    is_active = models.BooleanField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+# Bids
+class Bid(models.Model):
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE)
+    start_bid = models.FloatField()
+    final_bid = models.FloatField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)    
+
+# Comments
+class Comment(models.Model):
+    body = models.CharField(max_length=256)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Listing, on_delete=models.CASCADE)
