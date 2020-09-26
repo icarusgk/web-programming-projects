513db107523a0cb2ca8cd8ce74d3a1873aa9fc1c
Roger Castro <icarus.gk@gmail.com>
2020-09-26 17:31:02 -0600

    Change Listing

--- Commerce/auctions/models.py
+++ Commerce/auctions/models.py
@@ -5,12 +5,11 @@ from django.db import models
 class User(AbstractUser):
     pass
 
-# Listings
-class Listing(models.Model):
-    product_name = models.CharField(max_length=256)
-    description = models.CharField(max_length=256)
-    start_bid = models.FloatField(max_length=16)
-    image_url = models.URLField()
+# Auction Categories
+
+class Category(models.Model):
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE)
+    category = models.CharField(max_length=50)
 
 # Bids
 class Bid(models.Model):
@@ -23,7 +22,12 @@ class Comment(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     comment = models.CharField(max_length=256)
 
-# Auction Categories
-class Category(models.Model):
-    listing = models.ForeignKey(Listing, on_delete=models.CASCADE)
-    category = models.CharField(max_length=50)
+
+
+# Listings
+class Listing(models.Model):
+    product_name = models.CharField(max_length=256)
+    description = models.CharField(max_length=256)
+    start_bid = models.FloatField(max_length=16)
+    image_url = models.URLField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
