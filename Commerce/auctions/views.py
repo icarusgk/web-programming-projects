e8b24fb7e9238eaa5b84b9fb7e5bf52c09a1a1b8
Roger Castro <icarus.gk@gmail.com>
2020-10-05 23:43:11 -0600

    Add a remove view and Change name from 'user' to 'bid_user' for a less confusing name when rendered to the template

--- Commerce/auctions/views.py
+++ Commerce/auctions/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
 from django.db import IntegrityError
 from django.http import HttpResponse, HttpResponseRedirect
@@ -16,6 +17,7 @@ def index(request):
 	images = []
 	descriptions = []
 	price = []
+	is_active = []
 
 	for i in listing:
 		product_names.append(i.product_name)
@@ -23,17 +25,16 @@ def index(request):
 		images.append(i.image_url)
 		product_price = Bid.objects.get(listing=i.id)
 		price.append(product_price.final_bid)
+		is_active.append(i.is_active)
 
-	products = list(zip(product_names, descriptions, images, price))
+	products = list(zip(product_names, descriptions, images, price, is_active))
 
 
 	return render(request, "auctions/index2.html", {
 		"products": products
 	})
 
-
-def product(request, name):
-
+def content(request, name):
 	listing = list(Listing.objects.all())
 
 	product_names = []
@@ -43,25 +44,24 @@ def product(request, name):
 		product_names.append(i.product_name)
 		category.append(i.category.all())
 
-
-
 	if name in product_names:
 		product = Listing.objects.get(product_name=name)
 		name = product.product_name
 		image = product.image_url
 		description = product.description
-		user = product.user
+		bid_user = product.user
 		date = product.datetime
 		categories = product.category.values()
 		bid = Bid.objects.get(listing=product)
 		current_bids = bid.amount
 		comments = Comment.objects.filter(product=product)
+		is_active = product.is_active
 		
 		return render(request, 'auctions/content.html', {
 			"name": name,
 			"description": description,
 			"image": image,
-			"user": user,
+			"bid_user": bid_user,
 			"date": date,
 			"categories": categories,
 			"start_bid": bid.start_bid,
@@ -69,6 +69,7 @@ def product(request, name):
 			"current_bids": current_bids,
 			"comment": CommentForm(),
 			"comments": comments,
+			"is_active": is_active,
 			"new_bid": BidForm(initial={'new_bid': bid.final_bid})
 		})
 	else:
@@ -76,24 +77,26 @@ def product(request, name):
 			"error": f"This page '{name}' doesn't exist."
 		})
 
+@login_required
 def forms(request):
 	return render(request, "auctions/forms.html", {
 		"form": ListingForm()
 	})
 
+@login_required
 def input(request):
 	if request.method == "POST":
 		form = ListingForm(request.POST)
 		listing = request.POST['title']
-
+		user_name = request.POST['user_name']
 		if form.is_valid():
 			title = form.cleaned_data['title']
 			description = form.cleaned_data['description']
 			image_url = form.cleaned_data['image_url']
 			bid = form.cleaned_data['bid']
 			categories = form.cleaned_data['category']			
-									 
-			user = User.objects.get(username="icarus")
+			
+			user = User.objects.get(username = user_name)
 
 			new = Listing(
 				product_name = title, 
@@ -118,11 +121,12 @@ def bid(request):
 	if request.method == "POST":
 		form = BidForm(request.POST)
 		product = request.POST['page_name']
+		user_name = request.POST['user_name']
 
 		if form.is_valid():
 			bid = form.cleaned_data['new_bid']
 			current_listing = Listing.objects.get(product_name=product)
-			user = User.objects.get(username="paola")
+			user = User.objects.get(username=user_name)
 
 			new_bid = Bid.objects.get(listing=current_listing)
 
@@ -158,7 +162,18 @@ def comment(request):
 		else:
 			return HttpResponseRedirect(reverse("index"))
 
+def remove(request):
+	if request.method == "POST":
+		product = request.POST["remove_button"]
 
+		item = Listing.objects.get(product_name = product)
+
+		item.is_active = False
+		item.save()
+
+		return index(request)
+
+		
 def login_view(request):
 	if request.method == "POST":
 
