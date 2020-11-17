11105012b4634ac99dc885d5b8e701d0b3d52718
icarusgk <icarus.gk@gmail.com>
2020-11-17 12:46:05 -0600

    Add product view

--- new-commerce/auctions/views.py
+++ new-commerce/auctions/views.py
@@ -4,12 +4,71 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 
-from .models import User
-
+from .models import User, Listing, Bid, Watchlist, Comment
+from .forms import CommentForm, BidForm
 
 def index(request):
     return render(request, "auctions/index.html")
 
+def product(request, name):
+    listing = list(Listing.objects.all())
+
+    product_names = []
+    category = []
+
+    for i in listing:
+        product_names.append(i.product_name)
+        category.append(i.category.all())
+
+    if name in product_names:
+        product = Listing.objects.get(product_name = name)
+        name = product.product_name
+        image = product.image_url
+        description = product.description
+        bid_user = product.user
+        date = product.datetime
+        categories = product.category.all()
+        bid = Bid.objects.get(listing = product)
+        last_bid_user = bid.user
+        current_bids = bid.amount
+        is_active = product.is_active
+        current_user = User.objects.get(username = "icarus")
+        user_watchlist = Watchlist.objects.get(user = current_user)
+        comments_all = Comment.objects.filter(product = product)
+
+        comment_user = []
+        comment_content = []
+        categories_list = []
+        
+        for comment in comments_all:
+            comment_user.append(comment.user)
+            comment_content.append(comment.body)
+
+        comments = list(zip(comment_user, comment_content))
+
+        for category in categories:
+            categories_list.append(category.name)
+
+        return render(request, 'auctions/product.html', {
+            "name": name,
+            "description": description, 
+            "image": image,
+            "bid_user": bid_user,
+            "date": date,
+            "categories": categories_list,
+            "start_bid": bid.start_bid,
+            "final_bid": bid.final_bid,
+            "last_bid_user": last_bid_user,
+            "current_bids": current_bids,
+            "comment": CommentForm(),
+            "comments": comments,
+            "is_active": is_active,
+            "new_bid": BidForm(initial = {'new_bid': bid.final_bid})
+        })
+    else:
+        return render(request, 'auctions/content.html', {
+			"error": f"This page '{name}' doesn't exist."
+		})
 
 def login_view(request):
     if request.method == "POST":
