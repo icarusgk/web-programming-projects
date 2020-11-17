de20c952b654be89cb8f9f99b59a24fa574a74fa
icarusgk <icarus.gk@gmail.com>
2020-11-17 17:43:02 -0600

    Add missing urls

--- new-commerce/auctions/urls.py
+++ new-commerce/auctions/urls.py
@@ -4,6 +4,17 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("product/<str:name>", views.product, name="product"),
+    path("categories", views.categories, name="categories"),
+    path("category/<str:name>", views.category, name="category"),
+    path("add-watchlist", views.add_watchlist, name="add-watchlist"),
+    path("remove-watchlist", views.remove_watchlist, name="remove-watchlist"),
+    path("my-watchlist", views.my_watchlist, name="my-watchlist"),
+    path("forms", views.forms, name="forms"),
+    path("input", views.input, name="input"),
+    path("bid", views.bid, name="bid"),
+    path("remove", views.remove, name="remove"),
+    path("comment", views.comment, name="comment"),
     path("login", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout"),
     path("register", views.register, name="register")
