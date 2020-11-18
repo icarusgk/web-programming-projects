08264b2451e2b55f8de806d5262027cf8a4aec49
icarusgk <icarus.gk@gmail.com>
2020-11-18 14:26:42 -0600

    Add model support for admin interface

---
+++ new-commerce/auctions/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from .models import *
+
+class ListingAdmin(admin.ModelAdmin):
+    list_display = ("id", "product_name", "creator", "is_active")
+
+# Register your models here.
+admin.site.register(Listing, ListingAdmin)
+admin.site.register(Category)
+admin.site.register(Comment)
+admin.site.register(Bid)
+admin.site.register(Watchlist)
+admin.site.register(User)
+
