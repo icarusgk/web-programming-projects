35dd680051707434fb334c26cf7c57eed904a8b4
icarusgk <icarus.gk@gmail.com>
2020-11-19 17:51:25 -0600

    Add spaces to Models

--- new-commerce/auctions/models.py
+++ new-commerce/auctions/models.py
@@ -2,57 +2,63 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 import datetime as dt
 
+
 # Users
 class User(AbstractUser):
     pass
 
+
 # Category
 
 class Category(models.Model):
-    name = models.CharField(max_length = 50)
+    name = models.CharField(max_length=50)
 
     def __str__(self):
         return f"{self.name}"
 
-# Listing 
+
+# Listing
 
 class Listing(models.Model):
-    product_name = models.CharField(max_length = 256)
-    description = models.CharField(max_length = 256)
-    datetime = models.DateField(default = dt.datetime.now)
-    starting_price = models.FloatField(default = 0)
-    last_price = models.FloatField(default = 0)
-    image_url = models.URLField()  
+    product_name = models.CharField(max_length=256)
+    description = models.CharField(max_length=256)
+    datetime = models.DateField(default=dt.datetime.now)
+    starting_price = models.FloatField(default=0)
+    last_price = models.FloatField(default=0)
+    image_url = models.URLField()
     is_active = models.BooleanField()
     creator = models.ForeignKey(User, on_delete=models.CASCADE)
     category = models.ManyToManyField(Category)
-    
+
     def __str__(self):
         return f"{self.product_name} - uploaded by: '{self.creator}' on {self.datetime}"
 
-# Bids 
+
+# Bids
 class Bid(models.Model):
     listing = models.ForeignKey(Listing, on_delete=models.CASCADE)
     bid = models.FloatField()
-    user = models.ForeignKey(User, on_delete = models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return f"{self.listing.product_name} - ({self.user}) bidded: {self.bid}"
 
+
 # Comments
 
 class Comment(models.Model):
-    body = models.CharField(max_length = 256)
-    user = models.ForeignKey(User, on_delete = models.CASCADE)
-    product = models.ForeignKey(Listing, on_delete = models.CASCADE)
+    body = models.CharField(max_length=256)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Listing, on_delete=models.CASCADE)
 
     def __str__(self):
-        return f"{self.user} commmented on product ({self.product})"
+        return f"{self.user} commented on product ({self.product})"
+
 
 # Watchlist
 
 class Watchlist(models.Model):
-    user = models.ForeignKey(User, on_delete = models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     product = models.ManyToManyField(Listing)
 
     def __str__(self):
