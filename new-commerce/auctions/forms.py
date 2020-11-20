f67855099baf56e56b42abc3515f8cebdeb087df
icarusgk <icarus.gk@gmail.com>
2020-11-20 08:49:20 -0600

    Add forms.py

---
+++ new-commerce/auctions/forms.py
@@ -0,0 +1,20 @@
+from django import forms
+from .models import Category
+
+class ListingForm(forms.Form):
+    CHOICES = Category.objects.values_list()
+
+    title = forms.CharField(max_length=256)
+    description = forms.CharField(widget=forms.Textarea)
+    image_url = forms.URLField()
+    bid = forms.FloatField()
+    category = forms.MultipleChoiceField(
+        widget = forms.CheckboxSelectMultiple,
+        choices = CHOICES
+    )
+
+class CommentForm(forms.Form):
+    comment = forms.CharField(widget=forms.Textarea, max_length=256, label="")
+    
+class BidForm(forms.Form):
+    new_bid = forms.FloatField()
