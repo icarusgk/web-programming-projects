7df1bd56e44eb6c68d1dd9ff25be8e8675b686c8
icarusgk <icarus.gk@gmail.com>
2020-11-21 00:10:26 -0600

    Add User representation inside admin view

--- new-commerce/auctions/admin.py
+++ new-commerce/auctions/admin.py
@@ -4,11 +4,14 @@ from .models import *
 class ListingAdmin(admin.ModelAdmin):
     list_display = ("id", "product_name", "creator", "is_active")
 
+class UserAdmin(admin.ModelAdmin):
+    list_display = ("id", "username", "email", "date_joined")
+
 # Register your models here.
 admin.site.register(Listing, ListingAdmin)
 admin.site.register(Category)
 admin.site.register(Comment)
 admin.site.register(Bid)
 admin.site.register(Watchlist)
-admin.site.register(User)
+admin.site.register(User, UserAdmin)
 
