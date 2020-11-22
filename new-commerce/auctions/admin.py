911ebe3f70ebe9502dbe3684c1df9036d38bff52
icarusgk <icarus.gk@gmail.com>
2020-11-21 18:48:51 -0600

    Add CommentAdmin

--- new-commerce/auctions/admin.py
+++ new-commerce/auctions/admin.py
@@ -7,10 +7,13 @@ class ListingAdmin(admin.ModelAdmin):
 class UserAdmin(admin.ModelAdmin):
     list_display = ("id", "username", "email", "date_joined")
 
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ("user", "product", "time")
+
 # Register your models here.
 admin.site.register(Listing, ListingAdmin)
 admin.site.register(Category)
-admin.site.register(Comment)
+admin.site.register(Comment, CommentAdmin)
 admin.site.register(Bid)
 admin.site.register(Watchlist)
 admin.site.register(User, UserAdmin)
