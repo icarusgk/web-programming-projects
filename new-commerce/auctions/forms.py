c27651e8f489d5d7b2d5e54c0e2b67d610004bf3
icarusgk <icarus.gk@gmail.com>
2020-11-28 23:57:43 -0600

    Add placeholders and id to form fields

--- new-commerce/auctions/forms.py
+++ new-commerce/auctions/forms.py
@@ -5,9 +5,9 @@ from .models import Category
 class ListingForm(forms.Form):
     CHOICES = Category.objects.values_list()
 
-    title = forms.CharField(max_length=256)
-    description = forms.CharField(widget=forms.Textarea)
-    image_url = forms.URLField()
+    title = forms.CharField(max_length=256, widget=forms.TextInput(attrs={'placeholder': "Enter your product's title here", 'class': 'form-control'}))
+    description = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control', 'rows': '6'}))
+    image_url = forms.URLField(widget=forms.TextInput(attrs={'placeholder': "Enter your product's image URL here", 'class': 'form-control'}))
     bid = forms.FloatField()
     category = forms.MultipleChoiceField(
         widget=forms.CheckboxSelectMultiple,
