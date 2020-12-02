01e0cb776320bddb81a2afe205104e70a29a020e
Roger Castro <icarus.gk@gmail.com>
2020-12-01 23:41:17 -0600

    All Files

--- Commerce/auctions/urls.py
+++ Commerce/auctions/urls.py
@@ -4,18 +4,8 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("product/<str:name>", views.content, name="content"),
-    path("categories", views.categories, name="categories"),
-    path("category/<str:name>", views.category, name="category"),
-    path("add-watchlist", views.add_watchlist, name="add-watchlist"),
-    path("remove-watchlist", views.remove_watchlist, name="remove-watchlist"),
-    path("my-watchlist", views.my_watchlist, name="my-watchlist"),
-    path("forms", views.forms, name="forms"),
-    path("input", views.input, name="input"),
-    path("bid", views.bid, name="bid"),
-    path("remove", views.remove, name="remove"),
-    path("comment", views.comment, name="comment"),
+    path("product/<str:name>", views.product, name="product"),
     path("login", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout"),
-    path("register", views.register, name="register"),
+    path("register", views.register, name="register")
 ]
